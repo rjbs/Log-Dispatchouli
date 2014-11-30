rjbs/Log-Dispatchouli

commit 58d789e3015978ac746adc6525422e3702c6d77b

to_file init: simpler callback when file_format is unset
The callback for Log::Dispatch::File is now simplified (just one sub
instead of a sub wrapped with a sub) when file_format is not set. This
improves speed.

lib/Log/Dispatchouli.pm
@@ -174,19 +174,21 @@ sub new {
       }
     );
 
-    my $format = $arg->{file_format} || sub {
-      # The time format returned here is subject to change. -- rjbs,
-      # 2008-11-21
-      return (localtime) . ' ' . $_[0] . "\n"
-    };
-
     $log->add(
       Log::Dispatch::File->new(
         name      => 'logfile',
         min_level => 'debug',
         filename  => $log_file,
         mode      => 'append',
-        callbacks => sub { $format->({@_}->{message}) },
+        callbacks => do {
+          if (my $format = $arg->{file_format}) {
+            sub { $format->({@_}->{message}) }
+          } else {
+            # The time format returned here is subject to change. -- rjbs,
+            # 2008-11-21
+            sub { localtime . ' ' . {@_}->{message} . "\n" }
+          }
+        },
       )
     );
   }

commit 83147ca75bda85336becfaac509fbb590bd9830f

Use of "localtime" without parentheses is ambiguous
  Fix warning regression in 58d789e3015978ac746adc6525422e3702c6d77b

lib/Log/Dispatchouli.pm
@@ -186,7 +186,7 @@ sub new {
           } else {
             # The time format returned here is subject to change. -- rjbs,
             # 2008-11-21
-            sub { localtime . ' ' . {@_}->{message} . "\n" }
+            sub { (localtime) . ' ' . {@_}->{message} . "\n" }
           }
         },
       )